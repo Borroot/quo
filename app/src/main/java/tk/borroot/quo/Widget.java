f33d4f9ee5e2e2d061dffd5a964700b8523c2eeb
Borroot <borroot@live.nl>
2020-01-25 22:49:30 +0100

    Make the widget look nice.

--- app/src/main/java/tk/borroot/quo/Widget.java
+++ app/src/main/java/tk/borroot/quo/Widget.java
@@ -1,32 +1,65 @@
 package tk.borroot.quo;
 
+import android.annotation.SuppressLint;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class Widget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    private static final String TAG = Widget.class.getSimpleName();
+    private static final String[] symbols = {"L", "R"};
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
+    private String updateSymbol(Context context) {
+        // Create the shared pref object.
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                String.valueOf(R.string.shared_prefs), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        // Get the current index for the symbol from the shared pref.
+        int current = sharedPref.getInt(String.valueOf(R.string.saved_symbol_index), 0);
+        current = (current >= symbols.length - 1) ? 0 : current + 1;
+
+        // Update the index for the symbol.
+        editor.putInt(String.valueOf(R.string.saved_symbol_index), current);
+        editor.apply();
+
+        // Take the new symbol from the array.
+        return symbols[current % symbols.length];
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private String updateDate() {
+        return new SimpleDateFormat("HH:mm dd-MM").format(new Date());
+    }
+
+    private void updateWidget(Context context, AppWidgetManager widgetManager, int widgetId) {
+
+        String symbol = updateSymbol(context);
+        String date = updateDate();
+
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.app_widget_symbol, symbol);
+        views.setTextViewText(R.id.app_widget_date, date);
 
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        widgetManager.updateAppWidget(widgetId, views);
     }
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    public void onUpdate(Context context, AppWidgetManager widgetManager, int[] widgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        for (int widgetId : widgetIds) {
+            updateWidget(context, widgetManager, widgetId);
         }
     }
 
@@ -38,6 +71,11 @@ public class Widget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
     }
 }
 
