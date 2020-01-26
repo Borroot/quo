1330a8c218b5ba7b91ad7c0b220e70c90e1216f9
Borroot <borroot@live.nl>
2020-01-26 01:36:21 +0100

    Work in progress, receiving the onclick event.

--- app/src/main/java/tk/borroot/quo/Widget.java
+++ app/src/main/java/tk/borroot/quo/Widget.java
@@ -1,11 +1,13 @@
 package tk.borroot.quo;
 
 import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.text.SimpleDateFormat;
@@ -17,7 +19,9 @@ import java.util.Date;
 public class Widget extends AppWidgetProvider {
 
     private static final String TAG = Widget.class.getSimpleName();
-    private static final String[] symbols = {"L", "R"};
+
+    private static final String ACTION_WIDGET = "tk.borroot.quo.action.APPWIDGET_CLICK";
+    private static final String[] SYMBOLS = {"L", "R"};
 
     private String updateSymbol(Context context) {
         // Create the shared pref object.
@@ -27,14 +31,14 @@ public class Widget extends AppWidgetProvider {
 
         // Get the current index for the symbol from the shared pref.
         int current = sharedPref.getInt(String.valueOf(R.string.saved_symbol_index), 0);
-        current = (current >= symbols.length - 1) ? 0 : current + 1;
+        current = (current >= SYMBOLS.length - 1) ? 0 : current + 1;
 
         // Update the index for the symbol.
         editor.putInt(String.valueOf(R.string.saved_symbol_index), current);
         editor.apply();
 
         // Take the new symbol from the array.
-        return symbols[current % symbols.length];
+        return SYMBOLS[current % SYMBOLS.length];
     }
 
     @SuppressLint("SimpleDateFormat")
@@ -43,15 +47,21 @@ public class Widget extends AppWidgetProvider {
     }
 
     private void updateWidget(Context context, AppWidgetManager widgetManager, int widgetId) {
-
+        // Update the symbol and time variable.
         String symbol = updateSymbol(context);
         String date = updateDate();
 
+        // Set the variables in the remote view.
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-        views.setTextViewText(R.id.app_widget_symbol, symbol);
-        views.setTextViewText(R.id.app_widget_date, date);
+        views.setTextViewText(R.id.widget_symbol, symbol);
+        views.setTextViewText(R.id.widget_date, date);
 
-        // Instruct the widget manager to update the widget
+        // Set the onclick event for the widget.
+        Intent intent = new Intent(context, getClass()).setAction(ACTION_WIDGET);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+
+        // Instruct the widget manager to update the widget.
         widgetManager.updateAppWidget(widgetId, views);
     }
 
@@ -75,7 +85,11 @@ public class Widget extends AppWidgetProvider {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        super.onReceive(context, intent);
+        Log.d(TAG, "The button has been clicked with the action " + intent.getAction() + ".");
+        if (intent.getAction().equals(ACTION_WIDGET)) {
+            Log.d(TAG, "The button has been clicked by the widget action.");
+        }
     }
 }
 
